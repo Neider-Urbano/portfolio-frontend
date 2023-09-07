e986c7add8dd50ae1ffabba62d7c91637b36f5e4
Neider Urbano <jjuly8julianur@gmail.com>
2023-09-07 01:20:27 -0500

    config theme app

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
-import App from "./App";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import "./index.css";
+import ThemeProvider from "./providers/theme/ThemeProvider";
+import ThemeSwitcher from "./ThemeSwitcher";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <NextUIProvider>
-      <App />
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <ThemeProvider>
+          <ThemeSwitcher />
+        </ThemeProvider>
+      </NextThemesProvider>
     </NextUIProvider>
   </React.StrictMode>
 );
