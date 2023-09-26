8959aefca104839e5bc64a575d216f5b8db5b3c4
Neider Urbano <jjuly8julianur@gmail.com>
2023-09-25 23:35:53 -0500

    config typografy of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Epilogue", "sans-serif"],
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
